6512cb3c96f49ea4c322d1e5e6f4db06313d082c
marcuss <marcus.sanchez@contractors.birchbox.com>
2019-03-27 05:03:15 -0500

    90% working version.

--- app.py
+++ app.py
@@ -13,31 +13,221 @@
 #
 # NOTES:     The Idea of this program is to be extensible so in the
 #           future new Country/APIs can be query for company data.
-
+import os
+import logging.config
+import config
 from flask import Flask
 from flask import jsonify
 from flask import abort
 from flask import make_response
 from flask import request
-
+from flaskext.mysql import MySQL
+import pymysql
+import requests
 
 app = Flask(__name__)
-
+logging_conf_path = os.path.normpath(os.path.join(os.path.dirname(__file__), 'logging.conf'))
+logging.config.fileConfig(logging_conf_path)
+log = logging.getLogger(__name__)
 
 @app.route('/apiserver/v1/uk/company_info', methods=['GET'])
 def get_company_info():
     company_name = request.args.get('company_name')
-    if company_name == '178 - 202 LIMITED':
-        return jsonify(mockup_active)
-    if company_name == 'BANFORD CONSULTING LIMITED':
-        return jsonify(mockup_disolved)
+    company_number = request.args.get('company_number')
+
+    if not company_number:
+        if not company_name:
+            abort(400)
+
+    if company_number:
+        if company_name:
+            abort(400)
+
+    if  company_number:
+        if not company_name:
+            company =  get_company_by_number(company_number)
+            if not company:
+                abort(404)
+            else:
+                return company
+
+    if  company_name:
+        if not company_number:
+            company = get_company_by_name(company_name)
+            if not company:
+                abort(404)
+            else:
+                return company
     abort(404)
+
+def get_company_by_number(company_number):
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    try:
+        cursor.execute(
+            "SELECT * FROM company_data "
+            "WHERE company_data.CompanyNumber = %s ", (company_number,))
+        rows = cursor.fetchall()
+
+        if len(rows) == 0:
+            return null
+
+        companies = rows
+
+        cursor.execute(
+            "SELECT * FROM company_ann_reports "
+            "WHERE company_ann_reports.CompanyNumber = %s ", (company_number,))
+        rows = cursor.fetchall()
+
+        companies.append({'reports': rows})
+
+        cursor.execute(
+            "SELECT * FROM sync_payment_analysys "
+            "WHERE sync_payment_analysys.CompanyNumber = %s ", (company_number,))
+        single_row = cursor.fetchone()
+
+        companies.append({'analysys': single_row})
+
+        response = list()
+        response.append({'companies': companies})
+        return jsonify(response)
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+def get_company_by_name(company_name):
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    try:
+
+        #First search for part of the name
+        splitted = company_name.split()
+
+        if (len(splitted)>1): # companies with more than a word in the name
+            first2words = splitted[0] + " " + splitted[1] + " "
+
+            single_row = find_by_partial_name(cursor, first2words)
+
+            if single_row:
+                print("found by partial name.")
+                companies = list()
+                response = build_company_response(companies, cursor, single_row)
+                return jsonify(response)
+            else: #if not found by partial name.
+                first10 = company_name[0:10]
+                single_row = find_by_partial_name(cursor, first10)
+
+                if single_row: #
+                    print("found by first 10 letters.")
+                    companies = list()
+                    response = build_company_response(companies, cursor, single_row)
+                    return jsonify(response)
+
+                else: #if not found by first 10 letters.
+                    first5 = company_name[0:5]
+                    single_row = find_by_partial_name(cursor, first5)
+
+                    if single_row:
+                        print("found by first 5 letters.")
+                        companies = list()
+                        response = build_company_response(companies, cursor, single_row)
+                        return jsonify(response)
+
+                    else: #if not found by first 5 letters.
+                        return null
+        else: # companies with only a word in the name.
+            #full name search
+            single_row = find_by_partial_name(cursor, company_name)
+
+            if single_row:
+                print("found by full name search.")
+                companies = list()
+                response = build_company_response(companies, cursor, single_row)
+                return jsonify(response)
+            else: #not found with full name search.
+                return null
+
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def build_company_response(companies, cursor, single_row):
+    single_row['company_status']
+    companies.append(single_row)
+    company_number = extract_company_number(single_row)
+    rows = find_reports_by_company_number(company_number, cursor)
+    companies.append({'reports': rows})
+    single_row = find_analysys_by_company_number(company_number, cursor, single_row)
+    companies.append({'analysys': single_row})
+    response = list()
+    response.append({'companies': companies})
+    return response
+
+
+def extract_company_number(single_row):
+    return single_row.get("CompanyNumber", None)
+
+
+def find_analysys_by_company_number(company_number, cursor, single_row):
+    cursor.execute(
+        "SELECT * FROM sync_payment_analysys "
+        "WHERE sync_payment_analysys.CompanyNumber = %s ", (company_number,))
+    single_row = cursor.fetchone()
+    return single_row
+
+
+def find_reports_by_company_number(company_number, cursor):
+    cursor.execute(
+        "SELECT * FROM company_ann_reports "
+        "WHERE company_ann_reports.CompanyNumber = %s ", (company_number,))
+    rows = cursor.fetchall()
+    return rows
+
+
+def find_by_partial_name(cursor, partial):
+    cursor.execute(
+        "SELECT * FROM company_data "
+        "WHERE company_data.CompanyName LIKE %s ", (partial + '%',))
+    single_row = cursor.fetchone()
+    return single_row
+
+def get_company_live_status(company_number):
+    try:
+        print(config.CLIENT_SECRET)
+        print(config.LIVE_STATUS_ENDPOINT)
+        response =  requests.get(config.LIVE_STATUS_ENDPOINT+"/"+company_number,  auth=(config.CLIENT_SECRET, ''))
+        response.raise_for_status()
+        json_response = response.json()
+        return json_response['company_status']
+    except HTTPError as http_err:
+        print('HTTP exception occurred: {http_err}')
+        return null
+    except Exception as err:
+        print('Exception occurred: {err}')
+        return null
 
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify({'error': 'Invalid Request'}), 400)
+
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = config.MYSQL_DATABASE_USER
+app.config['MYSQL_DATABASE_PASSWORD'] = config.MYSQL_DATABASE_PASSWORD
+app.config['MYSQL_DATABASE_DB'] = config.MYSQL_DATABASE_DB
+app.config['MYSQL_DATABASE_HOST'] = config.MYSQL_DATABASE_HOST
+mysql.init_app(app)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=config.FLASK_DEBUG, port=config.SERVER_PORT)
---
+++ config.py
@@ -0,0 +1,13 @@
+# Flask settings
+FLASK_DEBUG = True  # Do not use debug mode in production
+
+SERVER_PORT = 5000
+
+CLIENT_SECRET = 'ybR9_pjwYjYZsCdKN4lV6qXkPvxejCkq712lenhG'
+LIVE_STATUS_ENDPOINT = 'https://api.companieshouse.gov.uk/company'
+
+MYSQL_DATABASE_USER  = 'clearsightapiext'
+MYSQL_DATABASE_PASSWORD  = 'jIj59f7*'
+MYSQL_DATABASE_DB  = 'clearsight_development'
+MYSQL_DATABASE_HOST  = '144.76.137.232'
+
